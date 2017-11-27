1809089fab7fbab4e15c92ff23daa8b3a35e4365
Juho Vepsalainen <bebraw@gmail.com>
2017-11-27 12:10:57 +0100

    chore: Set up image loading

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -30,6 +30,12 @@ const productionConfig = merge([
   parts.extractCSS({
     use: "css-loader",
   }),
+  parts.loadImages({
+    options: {
+      limit: 15000,
+      name: "[name].[ext]",
+    },
+  }),
 ]);
 
 const developmentConfig = merge([
@@ -39,6 +45,7 @@ const developmentConfig = merge([
     port: process.env.PORT,
   }),
   parts.loadCSS(),
+  parts.loadImages(),
 ]);
 
 module.exports = env => {
